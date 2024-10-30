2b25bac9be611d86660efbc43b86365be8354fc7
unknown <ekrapaulo29@gmail.com>
2024-10-30 10:53:58 +0000

    report-form

--- src/app/presentation/pages/report/report.component.ts
+++ src/app/presentation/pages/report/report.component.ts
@@ -1,12 +1,35 @@
-import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-report',
   standalone: true,
-  imports: [],
+  imports: [ReactiveFormsModule, CommonModule],
   templateUrl: './report.component.html',
   styleUrl: './report.component.css'
 })
-export class ReportComponent {
+export class ReportComponent implements OnInit{
+  formReport!: FormGroup;
+  type = [
+    {name: 'Élèves'},
+    {name: 'Professeurs'},
+    {name: 'Utilisateurs'},
+  ]
+  ngOnInit(): void {
+    this.loadForm()
+  }
+  loadForm(){
+    this.formReport = new FormGroup({
+      type: new FormControl('',Validators.required),
+    });
+  }
+  generateExcel() {
+    console.log(this.formReport.value);
+    };
+  generatePDF() {
+    console.log(this.formReport.value);
+
+  }
 
 }
